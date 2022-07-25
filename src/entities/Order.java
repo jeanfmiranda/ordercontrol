26a843fb1d058c6ee351bd746c7a4a97481f0b26
Jean Miranda <fjean.miranda@gmail.com>
2022-07-25 14:16:15 -0300

    updates in Order and OrderItem classes

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,32 @@
 package entities;
 
+import entities.enums.OrderStatus;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Order {
 
+    private Date moment;
+    private OrderStatus status;
+
+    private Client client;
+
+    private List<OrderItem>items = new ArrayList<>();
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+    }
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+    }
+
+    public Double total() {
+        double sum = 0;
+        for (OrderItem o : items) {
+            sum += o.subTotal();
+        }
+        return sum;
+    }
 }
